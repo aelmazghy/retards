bfb5812b7655641f1adc6ebed523b5197622a565
elmazghy <aelmazghy@gmail.com>
2020-03-03 09:04:44 +0100

    ini file

---
+++ settings.php
@@ -0,0 +1,53 @@
+<?php include ('header.php');
+
+
+?>
+
+
+
+<div class="breadcrumbs">
+    <div class="breadcrumbs-inner">
+        <div class="row m-0">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Accueil | Abdessamad</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Content -->
+<div class="content">
+
+    <!-- Animated -->
+    <div class="animated fadeIn">
+        <div class="profil">
+            <div class="feed-box text-center">
+                <section class="card">
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <div class="corner-ribon blue-ribon">
+                                <i class="fa fa-twitter"></i>
+                            </div>
+                                <img class="align-self-center rounded-circle mr-3" style="width:85px; height:85px;" alt="" src="images/admin.jpg">
+                            <div class="row form-group">
+                                <div class="col col-md-3"><label for="file-input" class=" form-control-label">Changer la photo</label></div>
+                                <div class="col-12 col-md-9"><input type="file" id="file-input" name="imageprofile" class="form-control-file"></div>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+
+    </div><!--end animated-->
+</div><!--end content-->
+<div class="clearfix"></div>
+
+
+<?php include ('footer.php');?>
+
+
